b556cf16272234eecf0b7f2e76ff6558d69c73eb
住江 祐哉 <yuya@sumienoMacBook-puro.local>
2019-05-16 10:37:56 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, FunctionComponent, useState } from 'react'
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Todo {
+  text: string
+  isClosed: boolean
 }
 
-export default App;
+const App: FunctionComponent = () => {
+  const [isAll, setIsAll] = useState(true)
+  const [items, setItems] = useState<Todo[]>([])
+  const [text, setText] = useState('')
+  const onAdd = () => {
+    setItems([...items, { text, isClosed: false }])
+    setText('')
+  }
+  const onClose = (text: string) => () => {
+    const index = items.findIndex(item => item.text === text)
+    const _items = [...items]
+    _items[index].isClosed = !_items[index].isClosed
+    setItems(_items)
+  }
+  const onDelete = (text: string) => () => {
+    const index = items.findIndex(item => item.text === text)
+    const _items = [...items]
+    _items.splice(index, 1)
+    setItems(_items)
+  }
+
+  return (
+    <Fragment>
+      <div>
+        <input onChange={event => setText(event.target.value)} value={text} />
+        <button
+          disabled={!text || -1 < items.findIndex(item => item.text === text)}
+          onClick={onAdd}
+        >
+          add
+        </button>
+      </div>
+      <div>
+        <button onClick={() => setIsAll(true)}>ALL</button>
+        <button onClick={() => setIsAll(false)}>Not Done</button>
+      </div>
+      <ul>
+        {items
+          .filter(item => isAll || !item.isClosed)
+          .map(item => (
+            <li key={item.text}>
+              <span>{item.isClosed ? <del>{item.text}</del> : item.text}</span>
+              <button onClick={onClose(item.text)}>{'done'}</button>
+              <button onClick={onDelete(item.text)}>{'x'}</button>
+            </li>
+          ))}
+      </ul>
+    </Fragment>
+  )
+}
+
+export default App
