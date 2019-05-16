d4bf8a4bdb92cf7b33bbcc70ef8419f011d6c70f
住江 祐哉 <yuya@sumienoMacBook-puro.local>
2019-05-16 21:19:47 +0900

    improve interface

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import React, { Fragment, FunctionComponent, useState } from 'react'
 interface Todo {
   text: string
   isClosed: boolean
+  isPinned: boolean
+  // action: string
 }
 
 const App: FunctionComponent = () => {
@@ -11,7 +13,7 @@ const App: FunctionComponent = () => {
   const [text, setText] = useState('')
   const [isDone, setIsDone] = useState(false)
   const onAdd = () => {
-    setItems([...items, { text, isClosed: false }])
+    setItems([...items, { text, isClosed: false , isPinned: false}])
     setText('')
   }
   const onClose = (text: string) => () => {
@@ -26,7 +28,12 @@ const App: FunctionComponent = () => {
     _items.splice(index, 1)
     setItems(_items)
   }
-
+  const onPin = (text: string) => () => {
+    const index = items.findIndex(item => item.text === text)
+    const _items = [...items]
+    _items[index].isPinned = !_items[index].isPinned
+    setItems(_items)
+  }
 
 
   function showState(){
@@ -40,7 +47,10 @@ const App: FunctionComponent = () => {
   }
   return (
     <Fragment>
-      <div>
+      <div className='title'>
+        <h1>TODO</h1>
+      </div>
+      <div className='input'>
         <input onChange={event => setText(event.target.value)} value={text} />
         <button
           disabled={!text || -1 < items.findIndex(item => item.text === text)}
@@ -49,23 +59,45 @@ const App: FunctionComponent = () => {
           add
         </button>
       </div>
-      <div>
-        <button onClick={() => {setIsAll(true)}}>ALL</button>
-        <button onClick={() => {setIsAll(false);setIsDone(false)}}>Not Done</button>
-        <button onClick={() => {setIsAll(false);setIsDone(true)}}>Done</button>
+      <div className='item'>
+      <div className='filter'>
+        <input type='radio' id='all' name='state' onClick={() => {setIsAll(true)}}></input>
+        <label htmlFor="all" className='tab'>all</label>
+        <input type='radio' name='state' id='notDone' onClick={() => {setIsAll(false);setIsDone(false)}}></input>
+        <label htmlFor="notDone" className='tab'>not Done</label>
+        <input type='radio' name='state' id='done' onClick={() => {setIsAll(false);setIsDone(true)}}></input>
+        <label htmlFor="done" className='tab'>Done</label>
       </div>
-      <h1>{showState()}</h1>
-      <ul>
-        {items
-          .filter(item => isAll || (isDone && item.isClosed) || (!isDone && !item.isClosed))
-          .map(item => (
-            <li key={item.text}>
-              <span>{item.isClosed ? <del>{item.text}</del> : item.text}</span>
-              <button onClick={onClose(item.text)}>{'done'}</button>
-              <button onClick={onDelete(item.text)}>{'x'}</button>
-            </li>
-          ))}
-      </ul>
+      <h2 className='state'>State:{showState()}</h2>
+
+        <h3 className='item_kind'>Pinned Item</h3>
+        <ul>
+          {items
+            .filter(item => (isAll || (isDone && item.isClosed) || (!isDone && !item.isClosed))&& item.isPinned)
+            .map(item => (
+              <li key={item.text}>
+                <span>{item.isClosed ? <del>{item.text}</del> : item.text}</span>
+                <button onClick={onClose(item.text)}>{item.isClosed? 'undo': 'done'}</button>
+                <button onClick={onDelete(item.text)}>{'x'}</button>
+                <button onClick={onPin(item.text)}>{'unPin'}</button>
+              </li>
+            ))
+          }
+        </ul>
+        <h3 className='item_kind'>Normal Item</h3>
+        <ul>
+          {items
+            .filter(item => (isAll || (isDone && item.isClosed) || (!isDone && !item.isClosed)) && !item.isPinned)
+            .map(item => (
+              <li key={item.text}>
+                <span>{item.isClosed ? <del>{item.text}</del> : item.text}</span>
+                <button onClick={onClose(item.text)}>{item.isClosed? 'undo': 'done'}</button>
+                <button onClick={onDelete(item.text)}>{'x'}</button>
+                <button onClick={onPin(item.text)}>{'Pin'}</button>
+              </li>
+            ))}
+        </ul>
+      </div>
     </Fragment>
   )
 }
